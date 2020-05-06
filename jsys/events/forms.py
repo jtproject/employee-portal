c2d6d3b2a4665b363377713caa3d83103ae355a3
Justin Thomas <jtprojecttemp@gmail.com>
2020-05-05 22:15:29 -0400

    updated event form

--- jsys/events/forms.py
+++ jsys/events/forms.py
@@ -1,11 +1,23 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
-from wtforms import StringField, SubmitField, SelectField, RadioField
+from wtforms import StringField, SubmitField, SelectField, RadioField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_login import current_user
 from jsys import utils
 
 class AddEventForm(FlaskForm):
-    month_year_opt = SelectField('Month/Year', validators=[DataRequired()], choices=[])
-    day_field = StringField('Day', validators=[DataRequired()])
+    event_my_opt = SelectField('Month/Year', validators=[DataRequired()], choices=[])
+    event_day = StringField('Day', validators=[DataRequired()])
+    event_name = StringField('Event Name', validators=[DataRequired()])
+    event_desc = TextAreaField('Rentals and Notes')
+    setup_my_opt = SelectField('Month/Year', choices=[])
+    setup_day = StringField('Day')
+    pickup_my_opt = SelectField('Month/Year', choices=[])
+    pickup_day = StringField('Day')
+    odd_setup = StringField('After Hour Setup')
+    odd_pickup = StringField('After Hour Pickup')
     submit = SubmitField('Add')
+    submit2 = SubmitField('Update')
+
+    def validate(self):
+        return True
--- jsys/events/routes.py
+++ jsys/events/routes.py
@@ -10,9 +10,30 @@ job = Blueprint('job', __name__)
 def events():
     return render_template('view_events.html', title='Upcoming Events')
 
-@job.route('/event/add')
+@job.route('/event/add', methods=['POST','GET'])
 def add_event():
     list_month_year = dataf.form_choices_mon_yr()
     form = AddEventForm()
-    form.month_year_opt.choices = list_month_year[0:12]
+    form.event_my_opt.choices = list_month_year[0:12]
+    form.setup_my_opt.choices = list_month_year[0:12]
+    form.pickup_my_opt.choices = list_month_year[0:12]
+    if form.validate_on_submit():
+        print(form.event_day.data, form.event_my_opt.data)
+        print(form.event_name.data, form.event_desc.data)
+        print(form.setup_my_opt.data, form.setup_day.data, form.odd_setup.data)
+        print(form.pickup_my_opt.data, form.pickup_day.data, form.odd_pickup.data)
+        flash(f'Go!')
+        return redirect(url_for('main.home'))
+    return render_template('add_event.html', title='Add Event', form=form)
+
+@job.route('/event/update', methods=['POST','GET'])
+def update_event():
+    form = AddEventForm()
+    form.event_my_opt.choices = [(1,1)]
+    #form.submit.label = '<label for="submit">Update</label>'
+    if form.validate_on_submit():
+        print('Mmoo0ooo')
+    elif request.method == 'GET':
+        form.event_name.data = 'YOOOOOOOOO'
+        print('zoommmmM')
     return render_template('add_event.html', title='Add Event', form=form)
